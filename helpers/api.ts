4d0adaede5f15a4c0e9d845748005f84b499ca11
chisomije92 <chisomije92@gmail.com>
2023-10-29 21:05:44 +0100

    install and set up axios

---
+++ helpers/api.ts
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import Router from 'next/router'
+
+const baseURL = "localhost:8000/api"
+
+const api = axios.create({
+    baseURL,
+});
+
+// Add a request interceptor
+api.interceptors.request.use(
+    (config) => {
+        const accessToken = localStorage.getItem('accessToken');
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+// Add a response interceptor
+api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+
+        // If the error status is 401 and there is no originalRequest._retry flag,
+        // it means the token has expired and we need to refresh it
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            try {
+                const refreshToken = localStorage.getItem('refreshToken');
+                const response = await axios.post('/api/refresh-token', { refreshToken });
+                const { accessToken } = response.data;
+
+                localStorage.setItem('accessToken', accessToken);
+
+                // Retry the original request with the new token
+                originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+                return axios(originalRequest);
+            } catch (error) {
+                // Handle refresh token error or redirect to login
+                // window.location.href = '/error'
+                Router.replace('/')
+                return Promise.reject(error);
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
+
+export default api
