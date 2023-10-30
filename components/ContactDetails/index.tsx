6ca922e9067e8d4ad3c9735163a815c385916ba1
chisomije92 <chisomije92@gmail.com>
2023-10-30 02:37:38 +0100

    fetch single contact

--- components/ContactDetails/index.tsx
+++ components/ContactDetails/index.tsx
@@ -1,44 +1,81 @@
 import ColorsComponent from "@/ui/ColorComponent";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../Header";
 import Footer from "../Footer";
+import api from "@/helpers/api";
+import { useRouter } from "next/router";
 
 const ContactDetails = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const [contact, setContact] = useState<{
+    id: number;
+    first_name: string;
+    last_name: string;
+    phone_number: string;
+  }>({
+    id: 0,
+    first_name: "",
+    last_name: "",
+    phone_number: "",
+  });
+
+  let [loading, setLoading] = useState(true);
+  let [color, setColor] = useState("#ffffff");
+  useEffect(() => {
+    const fetchContacts = async () => {
+      try {
+        const response = await api.get(`/contacts/${id}`);
+        setContact(response.data[0]);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchContacts();
+  }, [id]);
   return (
     <section>
       <Header />
       <ColorsComponent />
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Contact Details
-          </h2>
-        </div>
-      </div>
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-          <div className="bg-gray-100 h-40 w-full"></div>
-
-          <div className="p-6">
-            <div className="text-xl font-semibold">Chisom Ijeomah</div>
-            <div className="text-gray-500">555 - 555 - 555</div>
+      {!loading && contact.id && (
+        <>
+          {" "}
+          <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                {`${contact.first_name}'s contact`}
+              </h2>
+            </div>
           </div>
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+              <div className="bg-gray-100 h-40 w-full"></div>
 
-          <div className="flex justify-between p-6">
-            <Link
-              href={"/edit-contact"}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >
-              Edit
-            </Link>
-            <button className="bg-red-400 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-              Delete
-            </button>
+              <div className="p-6">
+                <div className="text-xl font-semibold">
+                  {contact.first_name} {contact.last_name}
+                </div>
+                <div className="text-gray-500">{contact.phone_number}</div>
+              </div>
+
+              <div className="flex justify-between p-6">
+                <Link
+                  href={"/edit-contact"}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Edit
+                </Link>
+                <button className="bg-red-400 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                  Delete
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+      )}
       <Footer />
     </section>
   );
