6b35297bb550d3f1e529a05a63d0c9790464ee9d
chisomije92 <chisomije92@gmail.com>
2023-10-30 02:55:11 +0100

    remove footer from home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Link from "next/link";
 import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 import ColorsComponent from "@/ui/ColorComponent";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -53,7 +52,6 @@ export default function Home() {
             />
           </div>
         </div>
-        <Footer />
       </main>
     </>
   );
